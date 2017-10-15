b0e56a9a381cb0f30a37e2836b1a9ddfec14be6b
dylan <342409840@qq.com>
2017-10-15 09:17:52 +0800

    2017

--- baiduMap/AppDelegate.m
+++ baiduMap/AppDelegate.m
@@ -13,6 +13,8 @@
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    //2017.10.15
+    
     // 要使用百度地图，请先启动BaiduMapManager
     _mapManager = [[BMKMapManager alloc]init];
     // 如果要关注网络及授权验证事件，请设定     generalDelegate参数
